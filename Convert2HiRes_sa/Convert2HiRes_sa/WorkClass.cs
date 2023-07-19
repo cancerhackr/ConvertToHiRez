eb3a734a9439f1e5bf7ba33d49f8d884440a1ae0
JAK <Jeff.Kempe@lhsc.on.ca>
2023-07-19 12:09:45 -0400

    Convert2HiRes_sa has a full initial stand-alone implementation of the script.

---
+++ Convert2HiRes_sa/Convert2HiRes_sa/WorkClass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VMS.TPS.Common.Model.API;
+
+namespace Convert2HiRes_sa
+{
+    internal class WorkClass
+    {
+        public static IEnumerable<object> Items { get; set; }
+        public static StructureSet StructureSet { get; set; }
+        private static IEnumerable<Structure> Structures => StructureSet.Structures;
+
+        public static void DoWork()
+        {
+            foreach (var item in Items)
+            {
+                Structure s = Structures.FirstOrDefault(str => str.Id == item.ToString());
+                if (s.IsHighResolution)
+                {
+                    continue;
+                }
+                if (s != null)
+                {
+                    if (s.CanConvertToHighResolution())
+                    {
+                        s.ConvertToHighResolution();
+                    }
+                }
+            }
+        }
+    }
+}
